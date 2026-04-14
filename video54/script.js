94fc5d40fa7887cbfc022e086d8b16fdb403a7ae
Anushka Napit <anushkanapit047@gmail.com>
2026-04-15 00:38:53 +0530

    Initial commit

---
+++ video54/script.js
@@ -0,0 +1,18 @@
+alert("hello world");
+
+console.log("hello world");
+console.log("code is running");
+console.log("code is running again");
+console.log("code is good");
+
+var a = prompt(" your number is");
+var istrue = confirm("are you sure?");
+
+if (istrue) {
+    console.log("computer is blasting " )
+} else {
+    console.log("computer is not blasting")
+}
+
+console.log("your number is " + a)
+
---
+++ video55/script.js
@@ -0,0 +1,21 @@
+console.log("this is video 55")
+
+let b = 25;
+let c = 4;
+let d = "anushka";
+let a = 20;
+// var 55a = 20; // this is not valid because variable name cannot start with a number  
+
+
+console.log(a + b  + c)
+console.log(a - b  + c)
+console.log(typeof a , typeof b , typeof c , typeof d)
+
+// const a1 = 6;
+// a1 = a1 + 1; // this will give an error because we cannot change the value of a constant variable
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 6;
